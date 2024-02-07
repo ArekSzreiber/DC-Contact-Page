d92ba5c2375f769e00dc1a0ee7b9d2ece315daec
Arkadiusz Szreiber <szreiber.arkadiusz@gmail.com>
2024-02-07 16:14:56 +0100

    add background

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import {ContactPageComponent} from './contact-page/contact-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '', component: ContactPageComponent,
+  },
+];
---
+++ src/app/contact-page/contact-page.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-page',
+  standalone: true,
+  imports: [],
+  styles: [``],
+  template: `
+    <p>
+      contact-page works!
+    </p>
+  `,
+})
+export class ContactPageComponent {
+
+}
